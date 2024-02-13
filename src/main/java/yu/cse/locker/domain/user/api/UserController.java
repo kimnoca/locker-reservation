54dc669d57a3151d6f05cd00dbdfbbca38798d5f
kimnoca <kimnoca@naver.com>
2024-02-13 11:53:36 +0900

    feat : User 엔티티 파일 추가

--- src/main/java/yu/cse/locker/domain/user/api/UserController.java
+++ src/main/java/yu/cse/locker/domain/user/api/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import yu.cse.locker.domain.user.application.UserService;
 import yu.cse.locker.domain.user.domain.User;
 import yu.cse.locker.domain.user.dto.RegisterRequestDto;
-import yu.cse.locker.domain.user.dto.UserDto;
 
 
 @RestController()
---
+++ src/main/java/yu/cse/locker/domain/user/domain/User.java
@@ -0,0 +1,92 @@
+package yu.cse.locker.domain.user.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @JsonIgnore
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "student_id")
+    private String studentId;
+
+    @Column(name = "student_name")
+    private String studentName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private Set<String> authorities = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.studentId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
